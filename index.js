d0c2f290d10500048edd6395b89276627afc8492
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-14 09:29:45 -0400

    commit tree 757ce42f24d96edb4d3994b4cc8b60aebf1bcf28

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob' //customer name in the global scope
+const leastFavoriteCustomer = 'sally'
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase() 
+}
+
+
+//Set Best Customer
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+//Overwrite Best Customer
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+//Least Favorite Customer
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Jackie'
+}
